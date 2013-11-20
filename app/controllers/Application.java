7a63a654dc17ab44e34f0986b97a8308b7a937d0
johaeuss <johaeuss@lenny.in.fhkn.de>
2013-11-20 11:44:53 +0100

    adding http request

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,44 +20,28 @@ public class Application extends Controller {
 	private static List<IHighscore> scoreList = new ArrayList<IHighscore>();
 
 	public static Result index() {
-//		String[] games = { "Pixelwars", "CoD", "VierGewinnt", "CoD",
-//				"VierGewinnt" };
-//		String[] player = { "Ich", "Du", "Er", "Sie", "Es" };
-//		Long[] score = { 1L, 0L, 500L, 100L, 1000L };
-//
-//		for (int i = 0; i < games.length; i++) {
-//			IHighscore s = new Highscore();
-//			s.setGame(games[i]);
-//			s.setPlayer(player[i]);
-//			s.setScore(score[i]);
-//			scoreList.add(s);
-//		}
-//		sortList();
+		// String[] games = { "Pixelwars", "CoD", "VierGewinnt", "CoD",
+		// "VierGewinnt" };
+		// String[] player = { "Ich", "Du", "Er", "Sie", "Es" };
+		// Long[] score = { 1L, 0L, 500L, 100L, 1000L };
+		//
+		// for (int i = 0; i < games.length; i++) {
+		// IHighscore s = new Highscore();
+		// s.setGame(games[i]);
+		// s.setPlayer(player[i]);
+		// s.setScore(score[i]);
+		// scoreList.add(s);
+		// }
+		// sortList();
 
 		return ok(index.render(scoreList));
 	}
 
-	public static Result addHighscore() {
-		IHighscore s = new Highscore();
-		try {
-			System.out.println(request().body().asJson());
-			s.deserializeJson(request().body().asJson());
-		} catch (HighscoreException e) {
-			return badRequest(e.getMessage() + "\n" + e.getStackTrace());
-		}
+	public static Result addHighscoreHttp(String game, String player, Long score) {
+		System.out.println(game + player + score);
 
-		for (int i = 0; i < scoreList.size(); i++) {
-			if (scoreList.get(i).getScore() < s.getScore()) {
-				scoreList.add(i, s);
-				break;
-			}
-		}
-		sortList();
-		return ok(index.render(scoreList));
-	}
-
-	public static Result addHighscore(String game, String player, Long score) {
 		IHighscore s = new Highscore();
+
 		s.setGame(game);
 		s.setPlayer(player);
 		s.setScore(score);
@@ -65,7 +49,21 @@ public class Application extends Controller {
 		sortList();
 		return ok(index.render(scoreList));
 	}
-	
+
+	public static Result addHighscoreJson() {
+		IHighscore s = new Highscore();
+        try {
+                System.out.println(request().body().asJson());
+                s.deserializeJson(request().body().asJson());
+        } catch (HighscoreException e) {
+                return badRequest(e.getMessage() + "\n" + e.getStackTrace());
+        }
+
+        scoreList.add(s);
+        sortList();
+        return ok(index.render(scoreList));
+	}
+
 	private static void sortList() {
 		Comparator<IHighscore> c = new Comparator<IHighscore>() {
 
@@ -73,8 +71,8 @@ public class Application extends Controller {
 			public int compare(IHighscore arg0, IHighscore arg1) {
 				long s1 = arg0.getScore();
 				long s2 = arg1.getScore();
-				
-				if(s1 == s2) {
+
+				if (s1 == s2) {
 					return 0;
 				} else if (s1 < s2) {
 					return 1;
@@ -82,7 +80,7 @@ public class Application extends Controller {
 					return -1;
 				}
 			}
-			
+
 		};
 		Collections.sort(scoreList, c);
 	}
